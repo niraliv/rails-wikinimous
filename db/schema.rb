266d2d3b631e27802d4773309e9b1a71bf704b82
Nirali Vasisht <nirali88@gmail.com>
2017-08-15 16:28:36 +0100

    exercise complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170815134003) do
+ActiveRecord::Schema.define(version: 20170815144352) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image_url"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,9 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
+10.times do
+  Article.create!(title: Faker::HarryPotter.character, content: Faker::HarryPotter.quote, image_url: "http://lorempixel.com/400/200/sports/")
+end
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
